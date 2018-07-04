bae4e4f0c72637c6a93787b42a86a3d839c9c005
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-07-04 12:52:48 +0200

    mixins

--- src/components/GalleryComponent.vue
+++ src/components/GalleryComponent.vue
@@ -45,12 +45,19 @@
 <script>
 
 import parts from '../data/parts';
+import createdHookMixin from '../mixins/created-hook-mixin';
 
 const headsFirstIndex = 0;
 const headsLastIndex = parts.heads.length - 1;
 
 export default {
     name: 'GalleryComponent',
+    // created() {
+    //     // https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
+    //     // Fetch ajax data is common thing to do in created
+    //     console.log('created');
+    // },
+    mixins: [createdHookMixin],
     data() {
         return {
             parts,
---
+++ src/mixins/created-hook-mixin.js
@@ -0,0 +1,7 @@
+export default {
+    created() {
+        // https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
+        // Fetch ajax data is common thing to do in created
+        console.log('created');
+    }
+}
